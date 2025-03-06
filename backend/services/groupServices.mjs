a61dbbb17a6fab0bc9fc2c689a08ad9a70ce044f
trankhanhnamhoccode <luyentapcoding@gmail.com>
2025-03-06 18:22:05 +0700

    fix:getFullGroupData unable to get

--- backend/services/groupServices.mjs
+++ backend/services/groupServices.mjs
@@ -239,7 +239,8 @@ const groupServices = {
             if (!groupData) {
                 return { message: "Group not found" };
             }
-            const members = groupData.members.slice(0,4).map(m => ({ avatar: m.avatar, role: m.role })) || [];
+            const members = groupData.members.slice(0,4).map(m => ({ user:m.user,avatar: m.avatar, role: m.role })) || [];
+            console.log(members)
             const userRole = members.find(m => m.user.toString() === userId.toString())?.role || "guest";
             const isJoined = members.some(m => m.user.toString() === userId.toString());
             console.log(groupData.private)
